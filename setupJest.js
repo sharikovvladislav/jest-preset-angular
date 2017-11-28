89f42109e3090c402d85ea0b933898e51e94946d
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-11-28 23:18:31 +0300

    feat: remove ng* attributes from

--- setupJest.js
+++ setupJest.js
@@ -8,7 +8,42 @@ require('zone.js/dist/sync-test');
 require('zone.js/dist/async-test');
 require('zone.js/dist/fake-async-test');
 require('jest-zone-patch');
+
 const AngularSnapshotSerializer = require('./AngularSnapshotSerializer');
+
+const MySerializer = {
+  test: AngularSnapshotSerializer.test,
+  print: (...rest) => {
+    const theResult = AngularSnapshotSerializer.print.call(this, ...rest);
+
+    const transformedResult = theResult.split('\n')
+        .map(val => {
+          if (val.includes('_ngcontent')) {
+            return val.replace(/_ngcontent-.*?$/, '').trim();
+          } else {
+            return val;
+          }
+        })
+        .map(val => {
+          if (val.includes('_nghost')) {
+            return val.replace(/_nghost-.*?$/, '').trim();
+          } else {
+            return val;
+          }
+        })
+        .map(val => {
+          if (val.includes('ng-reflect-')) {
+            return val.replace(/ng-reflect-.*?$/, '').trim();
+          } else {
+            return val;
+          }
+        })
+        .filter(val => !!val)
+        .join('\n');
+
+    return transformedResult;
+  }
+};
 const HTMLCommentSerializer = require('./HTMLCommentSerializer');
 const getTestBed = require('@angular/core/testing').getTestBed;
 const BrowserDynamicTestingModule = require('@angular/platform-browser-dynamic/testing').BrowserDynamicTestingModule;
@@ -21,3 +56,5 @@ getTestBed().initTestEnvironment(
 
 expect.addSnapshotSerializer(HTMLCommentSerializer);
 expect.addSnapshotSerializer(AngularSnapshotSerializer);
+
+expect.addSnapshotSerializer(MySerializer);
